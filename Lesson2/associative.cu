e339c5c8db671e79ae4ab47c4f9f362d3a274f26
Rachit Bhargava <rachitbits11@gmail.com>
2019-09-12 09:22:06 -0700

    Lesson 3 added

--- Lesson2/associative.cu
+++ Lesson2/associative.cu
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
+// COde to prove float addition is not associative.
 int main(int argc,char **argv)
-{   
-    printf("(%g + %g) + %g == %g\n%g + (%g + %g) == %g\n", 
-        1.f, 1e99, -1e99, (1.f + 1e99)+ -1e99, 
+{
+    printf("(%g + %g) + %g == %g\n%g + (%g + %g) == %g\n",
+        1.f, 1e99, -1e99, (1.f + 1e99)+ -1e99,
         1.f, 1e99, -1e99, 1.f + (1e99 + -1e99));
     return 0;
 }
